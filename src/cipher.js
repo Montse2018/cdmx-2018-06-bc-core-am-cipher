f693296bf9d3413d54ca04899224bb1c40a9e74f
Montse2018 <anmmtz@outlook.com>
2018-06-11 13:26:59 -0500

    primer actualizacion (cifrado)

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,37 @@
-window.cipher = {
+var text;
+var i;
+var newText;
+var string = [];
+var code = [];
+
+
+function encrypt (){
+    var text = document.getElementById("message").value;
+    var string = [];
+    for (var i = 0; i < text.length; i++) {
+        var character = String.fromCharCode((text [i]).charCodeAt()-65+13%26+65);
+        string.push(character);
+        var joinLetter = string.join("");
+      }
+      document.getElementById("encriptado").value = joinLetter;
+      return joinLetter;
+}
+
+    function unencript (){
+    var string = [];
+    var textoEncriptado = document.getElementById("textoEncriptado").value;
+    console.log(textoEncriptado);
+    var string = [];
+    for (var i = 0; i < textoEncriptado.length; i++) {
+        var character = String.fromCharCode((textoEncriptado [i]).charCodeAt()-65+13%26-65);
+        string.push(character);
+        var joinLetter2 = string.join("");
+      }
+      document.getElementById("desencriptado").value = joinLetter2;
+      return joinLetter2;
+}
+
+
+/*window.cipher = {
   // ...
-};
+};*/
