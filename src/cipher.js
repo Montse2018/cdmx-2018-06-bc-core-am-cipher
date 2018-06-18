36d7820b3c634862e85d34877e9e51583612d80d
Montse2018 <anmmtz@outlook.com>
2018-06-18 10:08:10 -0500

    ultima actualizacion

--- src/cipher.js
+++ src/cipher.js
@@ -29,7 +29,51 @@ window.cipher = {
   desencriptado.value = joinLetter2
     return joinLetter2;
   },
+  createCipherWithOffset: (offset) => {
+     objectWithOffset= {
+      encode: encodeOffset = cipher.encode(sting, offset),
+      decode: decodeOffset = cipher.decode(string, offset)
+    }
+
+    return objectwithOffset;
+  }
 };
+
+describe('cipher', () => {
+
+  it('debería ser un objeto', () => {
+    assert.equal(typeof cipher, 'object');
+  });
+
+  describe('cipher.encode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.encode, 'function');
+    });
+
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
+  });
+
+  describe('cipher.decode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+  });
+
+  describe('cipher.createCipherWithOffset', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.createCipherWithOffset, 'function');
+    });
+
+    it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado');
+
+  });
+
+});
 
     /*cipher = object,
     encode = function (),
--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ let encriptado = document.getElementById('encriptado');
 let desencriptado = document.getElementById('desencriptado');
 let buttonencode = document.getElementById('encode');
 let buttondecode = document.getElementById('decode');
+let encodeOffset = "";
+let decodeOffset = "";
+let objectWithOffset = document.getElementById('encode', 'decode');
+//let withOfset = document.getElementById('withOffset');
 
 
 //let encrypt = document.getElementById("Codificar");
@@ -17,9 +21,60 @@ let buttondecode = document.getElementById('decode');
       window.cipher.encode (offset.value, string.value)
   })
 
-  buttondecode.addEventListener("click", event =>{
+  buttondecode.addEventListener("click", event => {
      window.cipher.decode(offset.value, textoEncriptado.value)
   })
+
+
+  /*describe('cipher', () => {
+
+    it('debería ser un objeto', () => {
+      assert.equal(typeof cipher, 'object');
+    });
+
+    describe('cipher.encode', () => {
+
+      it('debería ser una función', () => {
+        assert.equal(typeof cipher.encode, 'function');
+      });
+
+      it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
+    });
+
+    describe('cipher.decode', () => {
+
+      it('debería ser una función', () => {
+        assert.equal(typeof cipher.decode, 'function');
+      });
+
+      it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+    });
+
+    describe('cipher.createCipherWithOffset', () => {
+
+      it('debería ser una función', () => {
+        assert.equal(typeof cipher.createCipherWithOffset, 'function');
+      });
+
+      it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado');
+
+    });
+
+  });*/
+
+  /*withOffset.addEventListener("click", withOffset) => {
+    function withOfset() {
+      obString.value;
+      resultado.innerHTML = window.cipher.createCipherWithOffset(offset.value);
+
+    }
+  }*/
+
+
+  /*function withOfset() {
+    obString.value;
+    resultado.innerHTML = window.cipher.createCipherWithOffset(offset.value);*/
+
   //let txtEncode =
 
 //   buttondecode.addEventListener("click",event => {
